50c0dd053b00b231f895b8b44ff23b074cdaca0e
Claudio Castillo <claudioandrescastillos@gmail.com>
2021-04-19 23:08:14 -0400

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,2 +1,2 @@
-<?php $dbh=mysql_connect ("localhost", "dciccs01_cristo", "d3st1n0") or die ('I cannot connect to the database because: ' . mysql_error());
-mysql_select_db ("dciccs01_cristianos"); ?>
+<?php $dbh=mysql_connect ("localhost", "db_name", "password") or die ('I cannot connect to the database because: ' . mysql_error());
+mysql_select_db ("db_name"); ?>
